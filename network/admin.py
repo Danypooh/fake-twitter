dca1e31f7770d1e8d6ea40364951063df4aa3c6a
Danypooh <delatorrechapell@gmail.com>
2024-09-05 15:00:13 -0600

    add models to django admin

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import User, Post, Media, Follower, Comment, Like, Retweet
+
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Post)
+admin.site.register(Media)
+admin.site.register(Follower)
+admin.site.register(Comment)
+admin.site.register(Like)
+admin.site.register(Retweet)
